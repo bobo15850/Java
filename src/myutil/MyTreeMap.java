f57a07d662f4cffee2c4a9eee42fd85688b5fda9
B_O_B <1248373163@qq.com>
2016-04-02 02:35:19 +0800

    完成紅黑树的左旋右旋，缺少调整颜色

--- src/myutil/MyTreeMap.java
+++ src/myutil/MyTreeMap.java
@@ -6,7 +6,24 @@ import java.util.Set;
 import java.util.SortedMap;
 
 /*
- * 通过树来实现map
+ * 通过红黑树来实现map
+ * 二叉查找树的性质：
+ * 1.如果左子树不为空，则左子树上所有的节点值小于根节点
+ * 2.如果右子树不为空，则右子树上所有的节点值大于根节点
+ * 3.任意节点的左右子树也都是二叉查找树
+ * 4.没有键值相等的节点
+ * 红黑树（二叉查找樹的一种）的性质：
+ * 1.每个节点要么红色要么黑色
+ * 2.根节点是黑色
+ * 3.每个叶节点（最后的null节点）都是黑色
+ * 4.如果一个节点是红色，那么他的两个子节点都是黑色
+ * 5.对任意节点：其道叶节点尾端每条路径包含相同数目的黑节点
+ * 
+ * 如果将二叉查找树的红色节点并入到黑色节点中，就是一个2，3,4树，可以经过证明得到其高度与节点总个数是log（n）关系
+ * 二三四数的插入不会往四节点中插入，删除不会删除一个二节点，以保证所有的叶子节点在同一层
+ * 
+ * 紅黑树的插入方式：1.直接通过2，3,4数来插入，插入的时候直接调整
+ * 				2.先插入二叉搜索树，在进行旋转和调整颜色
  */
 public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, Serializable {
 	private static final long serialVersionUID = -2987251376526992040L;
@@ -78,7 +95,7 @@ public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, S
 		else {
 			if (key == null) {
 				throw new NullPointerException();
-			}// 当没有指定比较器时treemap的键不鞥为空
+			}// 当没有指定比较器时treemap的键不能为空
 
 			// 此处限定了如果没有指定比较器的时候key必须实现comparable接口
 			@SuppressWarnings("unchecked")
@@ -104,7 +121,7 @@ public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, S
 		else {
 			parent.right = e;
 		}
-
+		fixAfterInsertion(e);// 对插入的值进行调整
 		// TODO要进行红黑树的调整，使其符合红黑树的性质
 
 		size++;
@@ -185,5 +202,81 @@ public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, S
 	public Set<MyMap.Entry<K, V>> entrySet() {
 		return null;
 	}
+
+	/*
+	 * 以下是支持旋转操作调整红黑树节点形态和颜色的静态操作
+	 */
+
+	private static <K, V> boolean colorOf(Entry<K, V> e) {
+		return e == null ? BLACK : e.color;
+	}// 返回节点的颜色，null默认为黑色因为叶节点都是null而且为黑色
+
+	private static <K, V> Entry<K, V> parentOf(Entry<K, V> e) {
+		return e == null ? null : e.parent;
+	}// 返回节点的父节点
+
+	private static <K, V> void setColor(Entry<K, V> e, boolean c) {
+		if (e != null) e.color = c;
+	}// 设置节点颜色
+
+	private static <K, V> Entry<K, V> leftOf(Entry<K, V> e) {
+		return e == null ? null : e.left;
+	}// 返回节点的左子树
+
+	private static <K, V> Entry<K, V> rightOf(Entry<K, V> e) {
+		return e == null ? null : e.right;
+	}// 返回节点的右子树
+
+	// 以下两种旋转操作只是改变树的形状，并不会改变颜色，不完全
+	private void rotateLeft(Entry<K, V> p) {
+		if (p != null) {
+			Entry<K, V> r = p.right;// 这个不会是空，因为如果这个是空的就没有左旋的必要了
+			p.right = r.left;
+			if (r.left != null) {
+				r.left.parent = p;
+			}
+			// 旋转之后右节点变成了顶节点,一下是设置顶节点的父节点的子节点
+			r.parent = p.parent;
+			if (p.parent == null) {
+				root = r;
+			}
+			else if (p.parent.left == p) {
+				p.parent.left = r;
+			}
+			else {
+				p.parent.right = r;
+			}
+			r.left = p;
+			p.parent = r;
+		}
+	}// 将该节点为根的子树左旋，需要调整三对父子关系
+
+	private void rotateRight(Entry<K, V> p) {
+		if (p != null) {
+			Entry<K, V> l = p.left;// 该节点不是是null，否则的话就没有必要进行右旋
+			p.left = l.right;
+			if (l.right != null) {
+				l.right.parent = p;
+			}
+
+			// 旋转之后左节点变成了顶节点
+			l.parent = p.parent;
+			if (p.parent == null) {
+				root = l;
+			}
+			else if (p.parent.left == p) {
+				p.parent.left = l;
+			}
+			else {
+				p.parent.right = l;
+			}
+			l.right = p;
+			p.parent = l;
+		}
+	}// 将该节点为根的子树右旋
+
+	private void fixAfterInsertion(Entry<K, V> e) {
+
+	}// 插入元素之后的调整，調用以上的方法实现
 
 }
