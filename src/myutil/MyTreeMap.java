ef14ca4b98503a40f9d629163407e8d73bbd4cb8
B_O_B <1248373163@qq.com>
2016-04-03 01:38:46 +0800

    完成操作节点操作

--- src/myutil/MyTreeMap.java
+++ src/myutil/MyTreeMap.java
@@ -501,7 +501,6 @@ public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, S
 			p.left = p.right = p.parent = null;// 消除引用防止内存泄漏，使其可以进行颜色调整
 			if (p.color == BLACK) {
 				fixAfterDeletion(replacement);
-				// TODO 进行删除节点后的调整，使其符合红黑树的性质5
 			}
 		}
 		else if (p.parent == null) {// p是根节点
@@ -522,8 +521,71 @@ public class MyTreeMap<K, V> extends MyAbstractMap<K, V> implements Cloneable, S
 		}
 	}// 刪除元素
 
+	// 调整要被删除的树，删除节点之前进行调整，参数是将要被删除的节点
 	private void fixAfterDeletion(Entry<K, V> x) {
+		// 只有删除的是黑色节点才需要调整，红色节点直接删除就好了
+		while (x != root && colorOf(x) == BLACK) {
+			// 被刪除的节点是其父节点的左节点
+			if (x == leftOf(parentOf(x))) {
+				Entry<K, V> sib = rightOf(parentOf(x));// 需要被刪除节点的兄弟节点（可以不存在，顔色为黑）
 
+				// 进行左旋
+				if (colorOf(sib) == RED) {
+					setColor(sib, BLACK);
+					setColor(parentOf(x), RED);
+					rotateLeft(parentOf(x));
+					sib = rightOf(parentOf(x));
+				}
+
+				// 需要被刪除节点的兄弟节点的两个子节点都是黑色
+				if (colorOf(leftOf(sib)) == BLACK && colorOf(rightOf(sib)) == BLACK) {
+					setColor(sib, RED);
+					x = parentOf(x);// 这里用while迭代代替了递归
+				}
+				else {
+					// TODO 这段代码不太理解是怎么进行操作的。。。
+					if (colorOf(rightOf(sib)) == BLACK) {
+						setColor(leftOf(sib), BLACK);
+						setColor(sib, RED);
+						rotateRight(sib);
+						sib = rightOf(parentOf(x));
+					}
+					setColor(sib, colorOf(parentOf(x)));
+					setColor(parentOf(x), BLACK);
+					setColor(rightOf(sib), BLACK);
+					rotateLeft(parentOf(x));
+					x = root;
+				}
+			}
+			else { // 被刪除的节点是其父节点的右节点，与上面相反
+				Entry<K, V> sib = leftOf(parentOf(x));
+
+				if (colorOf(sib) == RED) {
+					setColor(sib, BLACK);
+					setColor(parentOf(x), RED);
+					rotateRight(parentOf(x));
+					sib = leftOf(parentOf(x));
+				}
+
+				if (colorOf(rightOf(sib)) == BLACK && colorOf(leftOf(sib)) == BLACK) {
+					setColor(sib, RED);
+					x = parentOf(x);
+				}
+				else {
+					if (colorOf(leftOf(sib)) == BLACK) {
+						setColor(rightOf(sib), BLACK);
+						setColor(sib, RED);
+						rotateLeft(sib);
+						sib = leftOf(parentOf(x));
+					}
+					setColor(sib, colorOf(parentOf(x)));
+					setColor(parentOf(x), BLACK);
+					setColor(leftOf(sib), BLACK);
+					rotateRight(parentOf(x));
+					x = root;
+				}
+			}
+		}
 	}
 }
 /*
